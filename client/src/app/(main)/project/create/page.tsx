c3c41491cf5fee33aa671203aef9a5639d6b7d7e
Long Le <lhlong1542002@gmail.com>
2024-01-04 11:18:33 +0700

    added quill editor

--- client/src/app/(main)/project/create/page.tsx
+++ client/src/app/(main)/project/create/page.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import { InputBox, RichTextEditor } from "@/app/_components";
+
 const CreateProject = () => {
   return (
-    <div className="flex-1 w-full bg-red">
-      Project creation page
+    <div className="flex-1 w-full bg-white">
+      <InputBox 
+        inputName="projectTitle" 
+        placeholderText="Project Title" 
+        type="text" 
+        onChange={()=>{}}
+      />
+      <div className="flex mt-8">
+        <div className="w-1/3">Meta data section</div>
+        <div className="w-2/3">
+          <div>
+            <p className="text-2xl font-bold mb-4">Description</p>
+            <RichTextEditor />
+          </div>
+          <div>
+            <p className="text-2xl font-bold my-4">Tasks</p>
+            <RichTextEditor />
+          </div>
+          <p className="text-2xl font-bold my-4">References</p>
+          <RichTextEditor />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/app/_components/RichTextEditor.tsx
@@ -0,0 +1,44 @@
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+import "../globals.css"
+const QuillNoSSRWrapper = dynamic(() => import("react-quill"), { ssr: false });
+
+interface RichTextEditorProps {}
+
+const modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike"],
+    [{align: []}],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link"],
+    ["clean"],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
+const formats = [
+  "header", "font", "size",
+  "bold", "italic", "underline", "strike",
+  "align", "list", "bullet", "indent",
+  "link"
+];
+
+const RichTextEditor = ({}: RichTextEditorProps) => {
+  return (
+    <div className="h-fit">
+      <QuillNoSSRWrapper modules={modules} formats={formats} theme="snow" />
+    </div>
+  );
+};
+
+export default RichTextEditor;
