a8f052e8318f3bf304040c01d60d2f4134b37d85
LongLeGL <lhlong1542002@gmail.com>
2024-02-26 16:23:44 +0700

    handle side bar highlighting for administrate/users page

--- client/src/app/_components/SideBar.tsx
+++ client/src/app/_components/SideBar.tsx
@@ -21,73 +21,85 @@ interface SideBarItemProps {
 }
 
 const parseUrlString = (pathString: string) => {
-  const pathName = pathString.split('?')[0];
-  const param = pathString.split('?')[1];
-  const paramName = param.split('=')[0];
-  const paramVal = param.split('=')[1];
+  const pathName = pathString.split("?")[0];
+  const param = pathString.split("?")[1];
+  const paramName = param ? param.split("=")[0] : "";
+  const paramVal = param ? param.split("=")[1] : "";
 
-  return({path: pathName, paramName: paramName, paramVal: paramVal})
-}
+  return { path: pathName, paramName: paramName, paramVal: paramVal };
+};
 
 const SideBarItem = ({ Icon, title, paths, pages }: SideBarItemProps) => {
   const pathname = usePathname();
   const searchParam = useSearchParams();
 
-  let currPageBelongToPaths = paths.some((path)=>pathname.startsWith(path));
+  let currPageBelongToPaths = paths.some((path) => pathname.startsWith(path));
 
   const toggleAccordion = (accordBtn: any) => {
     let currTarget = accordBtn.parentNode.childNodes[1];
-    
-    if (currTarget.style.maxHeight == '0px'){
-      currTarget.style.maxHeight = '5em';
+
+    if (currTarget.style.maxHeight == "0px") {
+      currTarget.style.maxHeight = "8em";
     } else {
       currTarget.style.maxHeight = "0px";
     }
-
-  }
+  };
 
   var selectedOption = null;
   // console.log("Current path:",pathname);
   return (
     <div className="mb-2 w-4/5" key={title}>
-      <button className="font-medium w-full" onClick={(e)=>toggleAccordion(e.currentTarget)}>
+      <button
+        className="w-full font-medium"
+        onClick={(e) => toggleAccordion(e.currentTarget)}
+      >
         {currPageBelongToPaths ? (
           <div className="flex items-center gap-2 rounded-md bg-blue p-2 text-white ">
             {Icon && <Icon size={25} />}
             <span>{title}</span>
           </div>
         ) : (
-          <div className="flex items-center gap-2 px-2 py-1 text-gray hover:text-blue duration-300">
+          <div className="flex items-center gap-2 px-2 py-1 text-gray duration-300 hover:text-blue">
             {Icon && <Icon size={25} />}
             <span>{title}</span>
           </div>
         )}
       </button>
 
-      <div 
-        className= "ease-in-out duration-300 delay-150"
-        style={{maxHeight: currPageBelongToPaths ? '5em' : '0px', overflow:'hidden'}}
+      <div
+        className="delay-150 duration-300 ease-in-out"
+        style={{
+          maxHeight: currPageBelongToPaths ? "fit-content" : "0px",
+          overflow: "hidden",
+        }}
       >
-        {pages.map(function(accordLink: any){     // Process each subpage
+        {pages.map(function (accordLink: any) {
+          // Process each subpage
           let urlContent = parseUrlString(accordLink.href);
-          let selected = false
-          if (searchParam.has(urlContent.paramName) && searchParam.get(urlContent.paramName) == urlContent.paramVal)  
-            selected = true
+          let selected = false;
+          if (
+            (searchParam.has(urlContent.paramName) &&
+              searchParam.get(urlContent.paramName) == urlContent.paramVal) ||
+            (searchParam.size === 0 && urlContent.path === pathname)
+          )
+            selected = true;
 
-          return(
+          return (
             <div key={accordLink.href}>
-              <Link href={accordLink.href} className="block ml-4">
-                {selected ? 
-                  <span className="text-blue font-medium hover:text-blue">{accordLink.title}</span>
-                :
-                  <span className="text-gray hover:text-blue">{accordLink.title}</span>
-                }
-                
+              <Link href={accordLink.href} className="ml-4 block">
+                {selected ? (
+                  <span className="font-medium text-blue hover:text-blue">
+                    {accordLink.title}
+                  </span>
+                ) : (
+                  <span className="text-gray hover:text-blue">
+                    {accordLink.title}
+                  </span>
+                )}
               </Link>
             </div>
-          )
-        })
-        }
+          );
+        })}
       </div>
     </div>
   );
@@ -98,15 +110,19 @@ const SideBar = () => {
     {
       Icon: MdManageAccounts,
       title: "Administration",
-      paths: ["/administrate"],             // paths belonging to the sidebar tab option
+      paths: ["/administrate"], // paths belonging to the sidebar tab option
       pages: [
         {
           title: "Manage users",
-          href: "/administrate?manage=userManage",
+          href: "/administrate/users",
+        },
+        {
+          title: "Manage roles",
+          href: "/administrate/roles",
         },
         {
           title: "Set project registration deadline",
-          href: "/administrate?manage=registrationDeadline",
+          href: "/administrate/deadline",
         },
       ],
     },
@@ -128,7 +144,12 @@ const SideBar = () => {
     {
       Icon: FaProjectDiagram,
       title: "Projects",
-      paths: ['/project', '/project/create', '/project/approve', '/project/edit'],
+      paths: [
+        "/project",
+        "/project/create",
+        "/project/approve",
+        "/project/edit",
+      ],
       pages: [
         {
           title: "Specialized Projects",
@@ -158,7 +179,7 @@ const SideBar = () => {
     {
       Icon: VscGraph,
       title: "Evaluation",
-      paths: ['/evaluation'],
+      paths: ["/evaluation"],
       pages: [
         {
           title: "Input assessments",
@@ -180,7 +201,7 @@ const SideBar = () => {
 
       <div className="flex flex-col items-center">
         {sidebarItems.map(function (element) {
-          return <SideBarItem {...element} key={element.title}/>;
+          return <SideBarItem {...element} key={element.title} />;
         })}
       </div>
     </div>
