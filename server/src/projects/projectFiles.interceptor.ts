2efc4e81e2721725b7ceb6e4bd1288a1c6166f49
lonelyknight2902 <truongthinh2902@gmail.com>
2024-03-23 09:49:48 +0700

    feat(api): :sparkles: Introduce upload file api for project creation

--- server/src/projects/projectFiles.interceptor.ts
+++ server/src/projects/projectFiles.interceptor.ts
@@ -1,36 +1,41 @@
-import { Injectable, NestInterceptor, Type, mixin } from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
-import { diskStorage } from "multer";
-import path from "path";
+import { Injectable, NestInterceptor, Type, mixin } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import { diskStorage } from 'multer';
+import path from 'path';
 
 interface ProjectFilesInterceptorOptions {
   fieldName: string;
   path?: string;
 }
 
-function ProjectFilesInterceptor(options: ProjectFilesInterceptorOptions): Type<NestInterceptor> {
+function ProjectFilesInterceptor(
+  options: ProjectFilesInterceptorOptions,
+): Type<NestInterceptor> {
   @Injectable()
   class Interceptor implements NestInterceptor {
     fileInterceptor: NestInterceptor;
     constructor() {
-      const filesDestination = "./uploadedFiles";
-      const destination = `${filesDestination}${options.path}`
+      const filesDestination = './uploadedFiles';
+      const destination = `${filesDestination}${options.path}`;
 
       const multerOptions: MulterOptions = {
         storage: diskStorage({
           destination,
           filename: function (req, file, cb) {
             /*Appending extension with original name*/
-            cb(null, Date.now() + file.originalname)
-          }
-        })
-      }
+            console.log('saving');
+            cb(null, Date.now() + file.originalname);
+            console.log('done');
+          },
+        }),
+      };
 
-      this.fileInterceptor = new (FileInterceptor(options.fieldName, multerOptions));
+      this.fileInterceptor = new (FilesInterceptor(options.fieldName, null, multerOptions))();
     }
 
     intercept(...args: Parameters<NestInterceptor['intercept']>) {
+      console.log(args);
       return this.fileInterceptor.intercept(...args);
     }
   }
