d44fffc3689b1e3b693756da15a84a0b857edb93
LongLeGL <lhlong1542002@gmail.com>
2024-04-11 17:31:53 +0700

    display student of already enrolled in a non-approved project

--- client/src/app/_components/Modals/EnrollModal.tsx
+++ client/src/app/_components/Modals/EnrollModal.tsx
@@ -1,4 +1,3 @@
-import { projectData } from "@/app/(main)/project/projectData";
 import { useAuth } from "@/app/providers/AuthProvider";
 import { useProjects } from "@/app/providers/ProjectProvider";
 import { Text, Button } from "@mantine/core";
@@ -22,18 +21,28 @@ const EnrollModal = ({ targetProject }: { targetProject: Project }) => {
       .then(async (res) => {
         invalidateAndRefresh();
         handleUserEnrollProject(targetProject.code);
-        toggleNotification('Enroll Success', `Successfully enrolled into project ${targetProject.name}!`, 'success')
+        toggleNotification(
+          "Enroll Success",
+          `Successfully enrolled into project ${targetProject.name}!`,
+          "success",
+        );
         // console.log(`Enrolled into project ${targetProject.code}`)
       })
       .catch((err) => {
-        console.error("Error enrolling project:", err)
-        toggleNotification('Enroll Failed', `Cannot enroll into project!`, 'danger')
+        console.error("Error enrolling project:", err);
+        toggleNotification(
+          "Enroll Failed",
+          `Cannot enroll into project!`,
+          "danger",
+        );
       });
   }
 
   const openModal = async (e: SyntheticEvent, enrolledCode: number) => {
     e.stopPropagation();
-    let currentEnrolled = await axios.get(`http://localhost:3500/projects/${enrolledCode}`)
+    let currentEnrolled = await axios.get(
+      `http://localhost:3500/projects/${enrolledCode}`,
+    );
     modals.openConfirmModal({
       title: (
         <Text size="lg" c="blue" fw={600}>
@@ -42,20 +51,22 @@ const EnrollModal = ({ targetProject }: { targetProject: Project }) => {
       ),
       children: (
         <>
-          <Text size="sm">
-            You are already enrolled into project:
-          </Text>
-          <Text size="sm" c='blue'>
+          <Text size="sm">You are already enrolled into project:</Text>
+          <Text size="sm" c="blue">
             {currentEnrolled.data.code} - {currentEnrolled.data.name}
+            <span className="text-green-600">
+              {currentEnrolled.data.status === "APPROVED"
+                ? ""
+                : " (Undergoing approval process)"}
+            </span>
           </Text>
           <Text size="sm">
             Do you want to unenroll from it and enroll into new project:
           </Text>
-          <Text size="sm" c='blue'>
-          {targetProject.code} - {targetProject.name}?
+          <Text size="sm" c="blue">
+            {targetProject.code} - {targetProject.name}?
           </Text>
         </>
-        
       ),
       labels: { confirm: "Confirm", cancel: "Cancel" },
       onCancel: () => {},
@@ -63,19 +74,17 @@ const EnrollModal = ({ targetProject }: { targetProject: Project }) => {
     });
   };
 
-  return (
-    user?
+  return user ? (
     <Button
       onClick={(e) => {
         if (user.project?.code && user.project.code !== -1)
           openModal(e, user.project.code);
-        else
-          submitEnroll();
+        else submitEnroll();
       }}
     >
       Enroll
     </Button>
-    :
+  ) : (
     <span>Loading user</span>
   );
 };
