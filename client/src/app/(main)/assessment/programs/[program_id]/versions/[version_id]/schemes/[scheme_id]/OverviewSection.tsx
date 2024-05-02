673bc94ef2c51bdaa6e77c5e19b9db0feac31cc8
LongLeGL <lhlong1542002@gmail.com>
2024-05-02 09:46:56 +0700

    overview section for scheme details page

--- client/src/app/(main)/assessment/programs/[program_id]/versions/[version_id]/schemes/[scheme_id]/OverviewSection.tsx
+++ client/src/app/(main)/assessment/programs/[program_id]/versions/[version_id]/schemes/[scheme_id]/OverviewSection.tsx
@@ -1,9 +1,128 @@
-const OverViewSection = () => {
-	return(
-		<div>
-			overview
-		</div>
-	)
-}
+import {
+  Button,
+  Divider,
+  Grid,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { AiOutlineEdit } from "react-icons/ai";
+
+const OverViewSection = ({ schemeObject }: { schemeObject: any }) => {
+  return (
+    <div>
+      <div className="w-9/12">
+        <table style={{ width: "85%" }}>
+          <tr>
+            <td style={{ width: "25%", verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Scheme Name
+              </Text>
+            </td>
+            <td style={{ width: "75%" }}>
+              <Text size="md">{schemeObject.name}</Text>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Description
+              </Text>
+            </td>
+            <td>
+              <Text size="md">
+                Quisque rhoncus pretium pretium. Vestibulum ac ante et nisi
+                auctor efficitur in eget urna. Sed augue enim, tincidunt eu
+                tincidunt ac, sagittis at erat. Nam gravida, sapien a lobortis
+                consequat, dolor ante fringilla dolor, imperdiet bibendum ex mi
+                a turpis. Aliquam placerat sagittis semper. Integer urna sapien,
+                aliquet facilisis quam nec, aliquam hendrerit est. Integer in
+                dolor volutpat, fermentum magna a, cursus ex. Donec vel odio sed
+                dolor gravida auctor. Mauris semper posuere lectus vitae congue.
+                Nam nulla mi, fringilla eu euismod at, pretium ut nibh. Praesent
+                vitae nibh malesuada, interdum quam mattis, lobortis dui. Sed
+                facilisis tempor scelerisque.
+              </Text>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Assess Time
+              </Text>
+            </td>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md">
+                Year {schemeObject.assessTime.year} - Semester{" "}
+                {schemeObject.assessTime.no}
+              </Text>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Criteria Count
+              </Text>
+            </td>
+            <td>
+              <Text size="md">{schemeObject.criteriaCount}</Text>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Maximum Score
+              </Text>
+            </td>
+            <td>
+              <Text size="md">{schemeObject.maxScore}</Text>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ verticalAlign: "top" }}>
+              <Text size="md" fw={500}>
+                Last Modified
+              </Text>
+            </td>
+            <td>
+              <Text size="md">{schemeObject.lastModified}</Text>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <Divider my="md" />
+
+      <div>
+        <Text size="md" fw={500}>
+          Actions
+        </Text>
+        <div className="flex gap-4">
+          <Stack>
+            <Button w={"20rem"} leftSection={<AiOutlineEdit />}>
+              Edit Scheme
+            </Button>
+            <Button w={"20rem"} leftSection={<AiOutlineEdit />}>
+              Duplicate Scheme
+            </Button>
+            <Divider my="xs" w={"20rem"} />
+            <Button color="red" w={"20rem"} leftSection={<AiOutlineEdit />}>
+              Delete Scheme
+            </Button>
+          </Stack>
+          <Stack>
+            <Button w={"20rem"} leftSection={<AiOutlineEdit />}>
+              Export Assessment Form
+            </Button>
+            <Button w={"20rem"} leftSection={<AiOutlineEdit />}>
+              Export Scheme
+            </Button>
+          </Stack>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default OverViewSection;
--- client/src/app/(main)/assessment/programs/[program_id]/versions/[version_id]/schemes/[scheme_id]/page.tsx
+++ client/src/app/(main)/assessment/programs/[program_id]/versions/[version_id]/schemes/[scheme_id]/page.tsx
@@ -1,69 +1,92 @@
 "use client";
 import { PageHeader } from "@/app/_components";
 import React, { useEffect, useState } from "react";
-import { Accordion, Button } from "@mantine/core";
+import { Accordion, Button, ScrollArea } from "@mantine/core";
 import OverViewSection from "./OverviewSection";
 import RecordsSection from "./RecordsSection";
 import AttainmentsSection from "./AttainmentsSection";
 
 const SchemeDetail = ({ params }: { params: { scheme_id: string } }) => {
-  const [openedSections, setOpenedSections] = useState<string[] | undefined>(
-    [],
-  );
+  const [openedSections, setOpenedSections] = useState<string[] | undefined>([
+    "overview",
+    "records",
+    "attainments",
+  ]);
+  const [fetchedScheme, setFetchedScheme] = useState<any>();
 
+  useEffect(() => {
+    // Retreive scheme data
+    setFetchedScheme({
+      name: "Foundation test - Sem2",
+      description:
+        "Used for assessing student in foundation test semester 2 - year 2014",
+      assessTime: { year: 2014, no: 2 },
+      criteriaCount: 20,
+      maxScore: 50,
+      lastModified: "12/12/2013",
+    });
+  }, []);
+
+  if (!fetchedScheme) return <div>Fetching scheme data...</div>;
   return (
     <div className="flex h-full flex-col">
       <PageHeader pageTitle="Create Assessment Scheme" />
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <Button
-          ml={"auto"}
-          px={0}
-          py={0}
-          td={"underline"}
-          variant="transparent"
-          onClick={() => {
-            setOpenedSections(
-              openedSections?.length && openedSections?.length > 0
-                ? []
-                : ["overview", "records", "attainments"],
-            );
-          }}
-        >
-          {openedSections?.length && openedSections?.length > 0
-            ? "Collapse All"
-            : "Expand All"}
-        </Button>
-        <Accordion
-          radius="xs"
-          multiple
-          value={openedSections}
-          onChange={setOpenedSections}
-        >
-          <Accordion.Item value="overview">
-            <Accordion.Control style={{ borderBottom: "2px black solid" }}>
-              Overview
-            </Accordion.Control>
-            <Accordion.Panel>
-              schemeID: {params.scheme_id}
-              <OverViewSection />
-            </Accordion.Panel>
-          </Accordion.Item>
+      <ScrollArea type="auto" scrollbarSize={8} className="flex-1">
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <Button
+            ml={"auto"}
+            px={0}
+            py={0}
+            td={"underline"}
+            variant="transparent"
+            onClick={() => {
+              setOpenedSections(
+                openedSections?.length && openedSections?.length > 0
+                  ? []
+                  : ["overview", "records", "attainments"],
+              );
+            }}
+          >
+            {openedSections?.length && openedSections?.length > 0
+              ? "Collapse All"
+              : "Expand All"}
+          </Button>
+          <Accordion
+            radius="xs"
+            multiple
+            value={openedSections}
+            onChange={setOpenedSections}
+          >
+            <Accordion.Item value="overview">
+              <Accordion.Control style={{ borderBottom: "2px black solid" }}>
+                Overview
+              </Accordion.Control>
+              <Accordion.Panel>
+                schemeID: {params.scheme_id}
+                <OverViewSection schemeObject={fetchedScheme} />
+              </Accordion.Panel>
+            </Accordion.Item>
 
-          <Accordion.Item value="records">
-            <Accordion.Control style={{ borderBottom: "2px black solid" }}>
-              Assessment Records
-            </Accordion.Control>
-            <Accordion.Panel><RecordsSection /></Accordion.Panel>
-          </Accordion.Item>
+            <Accordion.Item value="records">
+              <Accordion.Control style={{ borderBottom: "2px black solid" }}>
+                Assessment Records
+              </Accordion.Control>
+              <Accordion.Panel>
+                <RecordsSection />
+              </Accordion.Panel>
+            </Accordion.Item>
 
-          <Accordion.Item value="attainments">
-            <Accordion.Control style={{ borderBottom: "2px black solid" }}>
-              PI Achievements
-            </Accordion.Control>
-            <Accordion.Panel><AttainmentsSection /></Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-      </div>
+            <Accordion.Item value="attainments">
+              <Accordion.Control style={{ borderBottom: "2px black solid" }}>
+                PI Achievements
+              </Accordion.Control>
+              <Accordion.Panel>
+                <AttainmentsSection />
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
